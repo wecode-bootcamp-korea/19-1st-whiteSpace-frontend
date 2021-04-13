3aa06790efcd432edd1f2492effe4fd86ee8552f
kdh24 <kdgus24@gmail.com>
2021-04-13 13:25:50 +0900

    Add : Nav 레이아웃 완료

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -4,20 +4,28 @@ import './Nav.scss';
 export class Nav extends Component {
   render() {
     return (
-      <div className="navWrapper">
+      <div id="nav">
         <h1>여백 0100</h1>
-        <ul>
-          <li>ALL</li>
-          <li>패브릭</li>
-          <li>주방/욕실</li>
-          <li>생활/홈케어</li>
-        </ul>
-        <ul>
-          <li>로그인</li>
-          <li>회원가입</li>
-          <li>장바구니</li>
-          <li></li>
-        </ul>
+        <div className="navLeftMenu">
+          <ul>
+            <li>ALL</li>
+            <li>패브릭</li>
+            <li>주방/욕실</li>
+            <li>생활/홈케어</li>
+          </ul>
+        </div>
+        <div className="navRightMenu">
+          <ul>
+            <li>로그인</li>
+            <li>회원가입</li>
+            <li>장바구니</li>
+            <li>
+              <button>
+                <i class="fas fa-search"></i>
+              </button>
+            </li>
+          </ul>
+        </div>
       </div>
     );
   }
