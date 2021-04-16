6b1d87171d1c48f01f5da5f0a028c822ff3b2715
kdh24 <kdgus24@gmail.com>
2021-04-16 14:14:39 +0900

    SAVE : 중간저장

--- src/Components/Nav/Popup.js
+++ src/Components/Nav/Popup.js
@@ -7,11 +7,22 @@ export class Popup extends Component {
   constructor() {
     super();
     this.state = {};
+    this.firstSlide = React.createRef();
+    this.currentSlide = React.createRef();
   }
+
+  componentDidMount() {
+    this.firstSlide.current.classList.add(SHOWING_CLASS);
+  }
+
+  slide() {
+    // currentSlide =
+  }
+
   render() {
     return (
       <div id="Popup">
-        <div className="slider_item">
+        <div className="slider_item" ref={this.firstSlide}>
           <span>팝업1</span>
         </div>
         <div className="slider_item">
@@ -23,3 +34,14 @@ export class Popup extends Component {
 }
 
 export default Popup;
+
+const POPUP_DATA = [
+  {
+    content: '지금 회원가입시 여백 3천원 할인 쿠폰 발급',
+    color: '$blue',
+  },
+  {
+    content: '카카오 플친 추가시 2,000원 할인 쿠폰 지급!',
+    color: '$blue',
+  },
+];
